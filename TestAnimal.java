20221721b558ffef3891589d0191773d1532e0df
MateoFernandez <32834342+MateoFernandez@users.noreply.github.com>
2017-10-19 10:59:36 +0200

    Update TestAnimal.java

--- TestAnimal.java
+++ TestAnimal.java
@@ -19,7 +19,7 @@
         animal1.setNombre(teclado.nextline());
 
       //Pedir la edad del animal
-        System.out.print("Teclee la edad del animal en meses");
+        System.out.print("Teclee la edad del animal");
         int edad = teclado.nextInt();
 
       //Asignar edad al objeto animal1
@@ -28,6 +28,6 @@
       //Informar de los datos de animal1
         animal1.nace();
         System.out.println("Mi nombre es " + animal1.getNombre() +
-                           " y mi edad es " + animal1.getEdad() + " meses");     
+                           " y mi edad es " + animal1.getEdad() + " años");     
     }
   }
